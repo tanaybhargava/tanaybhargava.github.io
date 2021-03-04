84407faab526a1225502dc574a246e25fd84321f
Tanay Bhargava <tanaybhargava@gmail.com>
2021-03-03 22:42:25 -0800

    First build

---
+++ build/precache-manifest.95a511141ac2c9e70efdb636fe44eec0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8f3b01bdd9e08f1f46d0cab7884469",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f51d1142e7ec55e848a8",
+    "url": "/static/css/2.0552839e.chunk.css"
+  },
+  {
+    "revision": "77712e4901268a6c272a",
+    "url": "/static/css/main.77d456bf.chunk.css"
+  },
+  {
+    "revision": "f51d1142e7ec55e848a8",
+    "url": "/static/js/2.18be11a4.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.18be11a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77712e4901268a6c272a",
+    "url": "/static/js/main.05e370c0.chunk.js"
+  },
+  {
+    "revision": "7ee3110ffc40fbafb97f",
+    "url": "/static/js/runtime-main.4a4f10e1.js"
+  },
+  {
+    "revision": "4e45ccee8e2c14411b1e033f008bc800",
+    "url": "/static/media/border_lemon.4e45ccee.png"
+  },
+  {
+    "revision": "6d119c9c3ba541e501c082c9859c9dee",
+    "url": "/static/media/header_large.6d119c9c.png"
+  }
+]);
