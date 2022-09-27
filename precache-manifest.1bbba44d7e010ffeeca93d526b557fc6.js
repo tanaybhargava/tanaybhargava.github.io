4813858d10e226630e5f10c54cf2d63cffc92e74
tanaybhargava <tanaybhargava@gmail.com>
2022-09-27 13:51:56 -0700

    Build 31

---
+++ precache-manifest.1bbba44d7e010ffeeca93d526b557fc6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ea53f25453d7c00e1560989946fd73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11a1e88f1a063bca0dc6",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "4f2adcc2b2645943f392",
+    "url": "/static/css/main.881ef7ea.chunk.css"
+  },
+  {
+    "revision": "11a1e88f1a063bca0dc6",
+    "url": "/static/js/2.cbe3521b.chunk.js"
+  },
+  {
+    "revision": "d114f163d78ec6d579edc27c111aa50e",
+    "url": "/static/js/2.cbe3521b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f2adcc2b2645943f392",
+    "url": "/static/js/main.e6eac6b4.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.e6eac6b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ca3a21b073142048ccc",
+    "url": "/static/js/runtime-main.a4c40521.js"
+  },
+  {
+    "revision": "758dc41bb9f97dc8cfb3e0c6dca06235",
+    "url": "/static/media/ColinTanayEngagement_Discovery1Edited.758dc41b.jpg"
+  },
+  {
+    "revision": "c0ae52cf72a9e7095e4d7f1b500c762d",
+    "url": "/static/media/border_colin.c0ae52cf.png"
+  }
+]);
